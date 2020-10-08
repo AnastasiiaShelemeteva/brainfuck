7d3dceb812ce008480dc58e467bb7a67ab94a651
Shelemeteva Anastasiia <anastasiia.shelemeteva@home24.de>
2020-10-08 16:06:58 +0200

    Now with loop and output

--- interpreter.go
+++ interpreter.go
@@ -20,39 +20,41 @@ func main() {
 	mem := []int{0}
 	memPos := 0
 
-	inputVar := ("+++++[>+>++>+++>++++>+++++<<<<<-]>-.>+++.+++..---.--.>>-.<++.>>++.>+.<<<<+++.+.")
+	inputVar := ("++++++++++[>+++++++++>++++++++++>+++>+++++++++++>+++++++>++++++++++++<<<<<<-]>---.>+++++.+++..---.--------.>>-.<++.>>++.>+.<<<<+++.+. ")
 	toArray := strings.Split(inputVar, "")	
 	for prgPos < len(toArray) {
 		if toArray[prgPos] == ">" {
 			memPos += 1
-			if len(mem) <= prgPos {
+			if len(mem) <= memPos {
 				mem = append(mem, 0)
 			}
-			fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos])
+			
 		} else if toArray[prgPos] == "<" {
 			memPos -= 1
-			fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos])
+			
 		} else if toArray[prgPos] == "+" {
 			mem[memPos] += 1
-			fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos])
+			
 		} else if toArray[prgPos] == "-" {
 			mem[memPos] -= 1
-			fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos])
+			
+		} else if toArray[prgPos] == "." {
+			fmt.Println("OUR ELEMENT IS ", string(mem[memPos]), "our memPos is", memPos, "symbol is ", toArray[prgPos])
 		} else if toArray[prgPos] == "[" {
-			fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos])
+			
 			if mem[memPos] == 0 {
 				counterOpen := 0
 				prgPos += 1
-				fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos], "counterOpen is ", counterOpen)
+				
 				for prgPos < len(toArray) {
 					if toArray[prgPos] == "]" && counterOpen == 0 {
-						fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos], "counterOpen is ", counterOpen)
+						
 						break
 					} else if  toArray[prgPos] == "[" {
 						counterOpen += 1
-						fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos], "counterOpen is ", counterOpen)
+						
 					} else if toArray[prgPos] == "]" {
-						fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos], "counterOpen is ", counterOpen)
+						
 						counterOpen -= 1
 					} 
 					prgPos += 1
@@ -60,11 +62,11 @@ func main() {
 				
 			} 
 		} else if toArray[prgPos] == "]" {
-			fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos])
+			
 			if mem[memPos] != 0 {
 				for toArray[prgPos] != "[" {
 					prgPos -= 1
-					fmt.Println("our memPos is", memPos, "symbol is ", toArray[prgPos], "memory is ", mem[memPos])
+					
 			/* 	} else {
 					maybe here should be something or not
 				} */
