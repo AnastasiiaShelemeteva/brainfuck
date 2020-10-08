2af12b41a35d32a1141d18844cab772fa1afa393
Shelemeteva Anastasiia <anastasiia.shelemeteva@home24.de>
2020-10-08 17:47:23 +0200

    Now it reads the file from the inside of code

--- interpreter.go
+++ interpreter.go
@@ -3,8 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -12,53 +13,56 @@ func main() {
 	mem := []int{0}
 	memPos := 0
 
-	inputVar := ("+++++++++++++++++++++++++++++++++.>,.")
-	toArray := strings.Split(inputVar, "")
+	inputVar, err := ioutil.ReadFile("test.bf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	toArray := inputVar
 	for prgPos < len(toArray) {
-		if toArray[prgPos] == ">" {
+		if toArray[prgPos] == 62 {
 			memPos++
 			if len(mem) <= memPos {
 				mem = append(mem, 0)
 			}
 
-		} else if toArray[prgPos] == "<" {
+		} else if toArray[prgPos] == 60 {
 			memPos--
 
-		} else if toArray[prgPos] == "+" {
+		} else if toArray[prgPos] == 43 {
 			mem[memPos]++
 
-		} else if toArray[prgPos] == "-" {
+		} else if toArray[prgPos] == 45 {
 			mem[memPos]--
 
-		} else if toArray[prgPos] == "." {
+		} else if toArray[prgPos] == 46 {
 			fmt.Print(string(mem[memPos]))
-		} else if toArray[prgPos] == "," {
+		} else if toArray[prgPos] == 44 {
 			fmt.Print("\nNo you need to write one letter. If you write more, only the first will count. Please, put here your letter: ")
 			scanner := bufio.NewScanner(os.Stdin)
 			scanner.Scan()
 			input := scanner.Text()
 			byteInput := []byte(input)
 			mem[memPos] = int(byteInput[0])
-		} else if toArray[prgPos] == "[" {
+		} else if toArray[prgPos] == 91 {
 			if mem[memPos] == 0 {
 				counterOpen := 0
 				prgPos++
 				for prgPos < len(toArray) {
-					if toArray[prgPos] == "]" && counterOpen == 0 {
+					if toArray[prgPos] == 93 && counterOpen == 0 {
 						break
-					} else if toArray[prgPos] == "[" {
+					} else if toArray[prgPos] == 91 {
 						counterOpen++
-					} else if toArray[prgPos] == "]" {
+					} else if toArray[prgPos] == 93 {
 						counterOpen--
 					}
 					prgPos++
 				}
 
 			}
-		} else if toArray[prgPos] == "]" {
+		} else if toArray[prgPos] == 93 {
 
 			if mem[memPos] != 0 {
-				for toArray[prgPos] != "[" {
+				for toArray[prgPos] != 91 {
 					prgPos--
 				}
 			}
