f201892b132748efb31eaf946f43f4cee3497ce5
Shelemeteva Anastasiia <anastasiia.shelemeteva@home24.de>
2020-10-12 10:12:07 +0200

    now everything works, but you call the file from the interpreter code itself, not from command line

--- interpreter.go
+++ interpreter.go
@@ -17,33 +17,41 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	toArray := inputVar
 	for prgPos < len(toArray) {
-		if toArray[prgPos] == 62 {
+		if toArray[prgPos] == 62 { // if command is ">"
 			memPos++
 			if len(mem) <= memPos {
 				mem = append(mem, 0)
 			}
 
-		} else if toArray[prgPos] == 60 {
+		} else if toArray[prgPos] == 60 { // if command is "<"
 			memPos--
 
-		} else if toArray[prgPos] == 43 {
+		} else if toArray[prgPos] == 43 { // if command is "+"
 			mem[memPos]++
+			if mem[memPos] > 255 {
+				mem[memPos] = 0
+			}
 
-		} else if toArray[prgPos] == 45 {
+		} else if toArray[prgPos] == 45 { // if command is "-"
 			mem[memPos]--
+			if mem[memPos] < 0 {
+				mem[memPos] = 255
+			}
 
-		} else if toArray[prgPos] == 46 {
+		} else if toArray[prgPos] == 46 { // if command is ":"
 			fmt.Print(string(mem[memPos]))
-		} else if toArray[prgPos] == 44 {
+		} else if toArray[prgPos] == 44 { // if command is ","
 			fmt.Print("\nNo you need to write one letter. If you write more, only the first will count. Please, put here your letter: ")
 			scanner := bufio.NewScanner(os.Stdin)
 			scanner.Scan()
 			input := scanner.Text()
 			byteInput := []byte(input)
 			mem[memPos] = int(byteInput[0])
-		} else if toArray[prgPos] == 91 {
+		} else if toArray[prgPos] == 91 { // if command is "["
+
 			if mem[memPos] == 0 {
 				counterOpen := 0
 				prgPos++
@@ -57,21 +65,25 @@ func main() {
 					}
 					prgPos++
 				}
-
 			}
-		} else if toArray[prgPos] == 93 {
-
+		} else if toArray[prgPos] == 93 { // if command is "]"
 			if mem[memPos] != 0 {
-				for toArray[prgPos] != 91 {
+				counterClosed := 0
+				prgPos--
+				for prgPos < len(toArray) {
+					if toArray[prgPos] == 91 && counterClosed == 0 {
+						break
+					} else if toArray[prgPos] == 93 {
+						counterClosed++
+					} else if toArray[prgPos] == 91 {
+						counterClosed--
+					}
 					prgPos--
 				}
 			}
-
 		}
+
 		prgPos++
-
 	}
-
-	fmt.Println("\nHere is an array: ", mem)
 
 }
